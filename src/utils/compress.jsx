884c58cebcef9ccdf29998d26d1f293740541dd2
Erudite <mooncrime93@gmail.com>
2023-06-27 19:13:25 +0900

    feat: lz-string库删除完毕

--- src/utils/compress.jsx
+++ src/utils/compress.jsx
@@ -1,21 +1,8 @@
-import * as LZString from 'lz-string'
-
-function decompressData(compressedString) {
-    let originString = LZString.decompress(compressedString);
-    return originString
-}
-
-function compressData(originString) {
-    let compressedString = LZString.compress(originString);
-    return compressedString;
-}
-
 function decompressCategories() {
     let resultList = []
     let compressedString = document.getElementById("DictWiki-Category").innerText;
     if (compressedString != '') {
-        let originString = decompressData(compressedString)
-        resultList = JSON.parse(originString)
+        resultList = JSON.parse(compressedString)
     }
     return resultList
 }
@@ -23,7 +10,7 @@ function decompressCategories() {
 function compressCategories(resultList) {
     let compressedString = "";
     if (resultList.length > 0) {
-        compressedString = compressData(JSON.stringify(resultList))
+        compressedString = JSON.stringify(resultList)
     }
     document.getElementById("DictWiki-Category").innerText = compressedString
 }
