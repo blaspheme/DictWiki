9a760b1ac86cdbf8e0a0aadeec9d2b6ed5d5efba
Erudite <mooncrime93@gmail.com>
2023-06-29 15:49:04 +0900

    feat: #2 WebDAV存储实现

--- src/utils/save.jsx
+++ src/utils/save.jsx
@@ -13,8 +13,33 @@ function getFileNameByURL() {
  * 下载HTML & WebDav保存
  */
 function saveData() {
-    downloadHTML() // 下载页面
-    changeState.value = false
+    if (window.location.protocol.includes('file')) {
+        downloadHTML() // 下载页面
+    } else {
+        saveByWebDAV()
+    }
+}
+
+/**
+ * 通过WebDAV保存
+ */
+function saveByWebDAV() {
+    // 保存的URL名称
+    let pathURL = window.location.origin + window.location.pathname;
+    var xhr = new XMLHttpRequest();
+    xhr.open("PUT", pathURL, true);
+    xhr.setRequestHeader("Content-Type", "text/html");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                alert("保存成功！");
+                changeState.value = false
+            } else {
+                alert("保存失败：" + xhr.status);
+            }
+        }
+    };
+    xhr.send(document.documentElement.outerHTML);
 }
 
 
@@ -27,6 +52,7 @@ function downloadHTML() {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+    changeState.value = false
 }
 
 export { saveData };
