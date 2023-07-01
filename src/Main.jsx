277b5d80fae3e90abba8c80aa4fe1018fb71feac
Erudite <mooncrime93@gmail.com>
2023-07-01 18:19:00 +0900

    style: 首页样式修改

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,6 +4,6 @@ import { ListWord } from "./pages/list/ListWord";
 export function Main() {
     return (<div class="main-container">
         <div class="container-column container-list-item full-width"><ListWord /></div>
-        <div class="container-column container-content"><Item /></div>
+        <div class="container-column"><Item /></div>
     </div>)
 }
